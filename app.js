4b243f86fdf3dc8a2a08ee7955234665e1616048
Alvee <35369563+0xMALVEE@users.noreply.github.com>
2022-10-11 19:58:23 +0600

    data updates

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cors = require('cors')
 var app = express()
 
 app.use(cors())
+app.use(express.static('public'))
 
 app.use("/advocates", require("./routes/advocates.js"))
 app.use("/companies", require("./routes/companies.js"))
