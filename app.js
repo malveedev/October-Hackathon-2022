700f734a686fe0a80670a7b27866e04de06db756
Alvee <35369563+0xMALVEE@users.noreply.github.com>
2022-10-11 19:11:20 +0600

    updates

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require("path")
 var cors = require('cors')
 var app = express()
 
@@ -7,6 +7,10 @@ app.use(cors())
 
 app.use("/advocates", require("./routes/advocates.js"))
 app.use("/companies", require("./routes/companies.js"))
+
+app.get("/", function(req,res){
+    res.sendFile( path.join(__dirname , "./error.html"))
+})
 
 app.listen(3000, function(){
     console.log("RUNNING!");
