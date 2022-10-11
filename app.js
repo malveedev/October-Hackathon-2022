4e4d6e0d75bf4f52c61b405861353741500aca9d
Alvee <35369563+0xMALVEE@users.noreply.github.com>
2022-10-11 19:27:28 +0600

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.get("/", function(req,res){
     res.sendFile( path.join(__dirname , "./error.html"))
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("RUNNING!");
 })
